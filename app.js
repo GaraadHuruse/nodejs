e223a900f90a6ba26257f98398a6df8c332ac062
GaraadHuruse <garaad.huruse@gmail.com>
2021-10-21 20:26:00 +0200

    NOFING

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const _ = require('lodash');
-const arrOfArr = [1, [2, [3, [4]]]]
-console.log( _.flattenDeep(arrOfArr))
+const {readFile, writeFile} = require('fs').promises;
+const util = require('util');
+const EventEmitter = require('events');
+
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const CustomEmitter = new EventEmitter();
+
+
+
+const start = async () => {
+    try{
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/async.txt', 'utf8');
+        await writeFile('./content/subfolder/merge', `
+        This file is merge of '/content/async.txt' and '/content/first.txt':
+        ${first}
+        ${second}
+        `, 'utf8')
+        console.log(first)
+        console.log(second)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+CustomEmitter.on('response', start);
+CustomEmitter.emit('response');
+console.log(CustomEmitter.eventNames())
